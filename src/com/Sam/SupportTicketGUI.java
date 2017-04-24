18deebbb9b61fd16c413375ac8d86ee275e51623
sagbeh <email@email.com>
2017-04-24 15:42:57 -0500

    Created GUI

--- src/com/Sam/SupportTicketGUI.java
+++ src/com/Sam/SupportTicketGUI.java
@@ -4,39 +4,72 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Date;
+import java.util.LinkedList;
 
 /**
  * Created by samagbeh on 3/29/17.
  */
-public class SupportTicketGUI extends JFrame{
+public class SupportTicketGUI extends JFrame {
+
+    LinkedList<Ticket> ticketQueue = new LinkedList<Ticket>();
+    LinkedList<Ticket> resolvedTickets = new LinkedList<Ticket>();
 
     private JMenuBar menuBar;
     private JPanel menuPanel;
     private JPanel rootPanel;
+    private JList<Ticket> TicketJList;
+    private JTextField topTextField;
+    private JTextField middleTextField;
+    private JTextField bottomTextField;
+    private JLabel topLabel;
+    private JLabel middleLabel;
+    private JLabel bottomLabel;
+    private JButton optionButton;
+    private JButton allTickets;
+    private JButton searchButton;
+    private JButton showAllResolvedTicketsButton;
 
-    protected SupportTicketGUI() {
+    DefaultListModel<Ticket> TicketListModel;
+
+    static Boolean addOrDelete = false;
+
+    public SupportTicketGUI() {
+
+
 
         super("Support Ticket System");
         setContentPane(rootPanel);
+        configureMenus();
         pack();
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
+        optionButton.setVisible(false);
+        topTextField.setVisible(false);
+        middleTextField.setVisible(false);
+        bottomTextField.setVisible(false);
+        topLabel.setVisible(false);
+        middleLabel.setVisible(false);
+        bottomLabel.setVisible(false);
+        searchButton.setVisible(false);
+
+    }
+
+    private void configureMenus() {
+
 
         menuBar = new JMenuBar();
         JMenu menu = new JMenu("Options");
         JMenuItem enterTicket = new JMenuItem("Enter Ticket");
-        JMenuItem deleteTicketID = new JMenuItem("Delete Ticket by ID");
-        JMenuItem deleteTicketIssue = new JMenuItem("Delete Ticket by Issue");
-        JMenuItem searchIssue = new JMenuItem("Search by Issue");
-        JMenuItem displayAllTickets = new JMenuItem("Display All Tickets");
+        JMenuItem deleteTicket = new JMenuItem("Delete a Ticket");
+        JMenuItem searchTicket = new JMenuItem("Search Open Tickets");
         JMenuItem quit = new JMenuItem("Quit");
-        menu.add(enterTicket);
-        menu.add(deleteTicketID);
-        menu.add(deleteTicketIssue);
-        menu.add(searchIssue);
-        menu.add(displayAllTickets);
-        menu.add(quit);
-        menuBar.add(menu);
+
+
+        TicketListModel = new DefaultListModel<>();
+        TicketJList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        TicketJList.setModel(TicketListModel);
+
 
         menuPanel.add(menuBar, BorderLayout.WEST);
 
@@ -44,11 +77,187 @@ public class SupportTicketGUI extends JFrame{
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                addOrDelete = true;
+
+                optionButton.setVisible(true);
+                topTextField.setVisible(true);
+                middleTextField.setVisible(true);
+                bottomTextField.setVisible(true);
+                topLabel.setVisible(true);
+                middleLabel.setVisible(true);
+                bottomLabel.setVisible(true);
+                searchButton.setVisible(false);
+
+
+                topLabel.setText("Enter a description of the issue:");
+                middleLabel.setText("Who reported the issue?");
+                bottomLabel.setText("What is the priority of this ticket?");
+                optionButton.setText("Add ticket");
+
+
             }
         });
+
+        deleteTicket.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                addOrDelete = false;
+
+                optionButton.setVisible(true);
+                topTextField.setVisible(false);
+                middleTextField.setVisible(true);
+                bottomTextField.setVisible(true);
+                topLabel.setVisible(false);
+                middleLabel.setVisible(true);
+                bottomLabel.setVisible(true);
+                searchButton.setVisible(true);
+
+
+                middleLabel.setText("Search open tickets by issue: ");
+                bottomLabel.setText("How was the issue resolved? ");
+                optionButton.setText("Delete ticket");
+
+            }
+        });
+
+
+        searchTicket.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                optionButton.setVisible(false);
+                topTextField.setVisible(false);
+                middleTextField.setVisible(true);
+                bottomTextField.setVisible(false);
+                topLabel.setVisible(false);
+                middleLabel.setVisible(true);
+                bottomLabel.setVisible(false);
+                searchButton.setVisible(true);
+
+                middleLabel.setText("Search open tickets by issue: ");
+
+            }
+        });
+
+
+        quit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        optionButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (addOrDelete == true) {
+
+                    Date dateReported = new Date(); //Default constructor creates Date with current date/time
+                    Date resDate = null;
+                    String fix = null;
+
+                    String problem = topTextField.getText();
+                    String reporter = middleTextField.getText();
+                    int priority = Integer.parseInt(bottomTextField.getText());
+
+
+                    Ticket t = new Ticket(problem, priority, reporter, dateReported, resDate, fix);
+                    //ticketQueue.add(t);
+                    TicketListModel.addElement(t);
+
+                    ticketQueue.add(t);
+
+                } else {
+
+
+                    Ticket toDelete = TicketJList.getSelectedValue();
+                    String resolution;
+
+
+                        resolution = bottomTextField.getText();
+                        if (resolution.equals("")) {
+                            JOptionPane.showMessageDialog(SupportTicketGUI.this, "Enter a resolution for the issue");
+                            return;
+                        }
+
+
+                    TicketListModel.removeElement(toDelete);
+                    ticketQueue.remove(toDelete);
+                    Date resDate = new Date();
+                    toDelete.setResDate(resDate);
+                    toDelete.setFix(resolution);
+                    resolvedTickets.add(toDelete);
+
+                }
+
+            }
+        });
+
+        allTickets.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TicketListModel.clear();
+
+                for (Ticket ticket : ticketQueue) {
+                    TicketListModel.addElement(ticket);
+                }
+
+            }
+        });
+
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ticketSearch();
+
+            }
+        });
+
+        showAllResolvedTicketsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TicketListModel.clear();
+
+                for (Ticket ticket : resolvedTickets) {
+                    TicketListModel.addElement(ticket);
+                }
+
+            }
+        });
+
+        menu.add(enterTicket);
+        menu.add(deleteTicket);
+        menu.add(searchTicket);
+        menu.add(quit);
+        menuBar.add(menu);
+
     }
 
+    public void ticketSearch() {
+
+        String ticketDescrip = middleTextField.getText();
 
 
+        LinkedList<Ticket> searchTicketList = new LinkedList<Ticket>();
+
+
+        TicketListModel.clear();
+
+        for (Ticket ticket : ticketQueue) {
+            if (ticket.getDescription().contains(ticketDescrip)) {
+                searchTicketList.add(ticket);
+
+            }
+
+        }
+
+        for (Ticket t : searchTicketList) {
+            TicketListModel.addElement(t);
+
+        }
+
+    }
 
 }
--- src/com/Sam/Ticket.java
+++ src/com/Sam/Ticket.java
@@ -101,8 +101,8 @@ public class Ticket {
     }
 
     public String toString(){
-        return("ID: " + this.ticketID + " Issue: " + this.description + " Priority: " + this.priority + " Reported by: "
-                + this.reporter + " Reported on: " + this.dateReported);
+        return("[" + "ID: " + this.ticketID + "]" + "["+ " Issue: " + this.description + "]" + "[" + " Priority: " + this.priority + "]"+ "[" + " Reported by: "
+                + this.reporter + "]"+ "[" + " Reported on: " + this.dateReported + "]"+ "[" + " Resolved on: " + this.resDate + "]"+ "["+ " Resolution: " + this.fix + "]");
     }
 }
 
